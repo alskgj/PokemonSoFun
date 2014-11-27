c15cd21cc0092633cf834a39d1a44afdab4dc0f8
zen <dwessels@ethz.ch>
2014-11-27 14:27:55 +0100

    2 guis > 1 gui

--- core/gui.py
+++ core/gui.py
@@ -16,38 +16,72 @@ class Tesla1(Frame):
         Frame.__init__(self, parent)
         self.pack(expand=YES, fill=BOTH)
 
+
         # entry
-        self.ent = Entry(self, width=50)
+        self.ent = Entry(self, width=50, relief=SUNKEN, font=('Courier New', 14, ))
         self.ent.insert(0, 'Enter Pokemon here [press enter to clear this]')
         self.ent.bind('<Return>', (lambda event: self.fetch()))
 
         # label - sprite
         png = PhotoImage(file="sprites/dragonite.png")
-        self.sprite = Label(self, image=png, bg="white")
+        self.sprite = Label(self, image=png, bg="LightYellow2")
         self.sprite.image = png  # prevent garbage collection?
 
         # label - stats
-        self.stats = Text(self, relief=SUNKEN, height=12, width=20, bg="darkgray")
+        self.stats = Text(self, relief=RIDGE, height=12, width=20, bg="darkgray")
         self.stats.config(font=('consolas', 14))
         self.stats.insert('1.0', "\nstats will be here\n")
 
         # label - name
-        self.name_label = Text(self, height=1, relief=SUNKEN, width=10)
+        self.name_label = Text(self, height=1, relief=SUNKEN, width=10, bg="wheat2")
         self.name_label.config(font=('consolas', 20, 'bold'))
         self.name_label.insert('1.0', "No pokemon selected")
 
         # label - type defenses
-        self.type_defenses = Text(self, relief=SUNKEN, height=12, width=70, bg="darkgray")
+        self.type_defenses = Text(self, relief=RIDGE, height=12, width=70, bg="darkgray")
         self.type_defenses.config(font=('consolas', 14))
         self.type_defenses.insert('1.0', open("Database/defaulttextstats.txt").read())
 
+        # second pokemon
+        self.create_second_pokemon_widgets()
+
+        # first pokemon
         self.ent.pack(side=TOP, fill=X, expand=NO)
-        self.ent.focus_set()
+        self.ent.focus()
         self.name_label.pack(side=TOP, fill=X, expand=NO)
         self.sprite.pack(side=RIGHT, fill=BOTH, expand=YES)
         self.stats.pack(side=LEFT, expand=YES, fill=BOTH)
         self.type_defenses.pack(side=RIGHT, expand=YES, fill=BOTH)
 
+        # second pokemon
+        self.name_label2.pack(side=TOP, fill=X, expand=NO)
+        self.sprite2.pack(side=RIGHT, fill=BOTH, expand=YES)
+        self.stats2.pack(side=LEFT, expand=YES, fill=BOTH)
+        self.type_defenses2.pack(side=RIGHT, expand=YES, fill=BOTH)
+
+        self.state = {
+            "pokemon": None
+        }
+
+
+    def create_second_pokemon_widgets(self):
+        # copy name, stats, type defenses and sprite to widget below
+        frame2 = Frame(self)
+        frame2.pack(side=BOTTOM, expand=YES, fill=BOTH)
+        self.name_label2 = Text(frame2, height=1, relief=SUNKEN, font=('consolas', 20, 'bold'), bg="wheat3")
+        self.name_label2.insert('1.0', "old pokemon")
+
+        self.stats2 = Text(frame2, relief=RIDGE, height=12, width=20, bg="darkgray")
+        self.stats2.config(font=('consolas', 14))
+        self.stats2.insert('1.0', "\nstats will be here\n")
+
+        self.type_defenses2 = Text(frame2, relief=RIDGE, height=12, width=70, bg="darkgray")
+        self.type_defenses2.config(font=('consolas', 14))
+        self.type_defenses2.insert('1.0', open("Database/defaulttextstats.txt").read())
+
+        png2 = PhotoImage(file="sprites/dragonite.png")
+        self.sprite2 = Label(frame2, image=png2, bg="LightYellow3")
+        self.sprite2.image = png2  # prevent garbage collection?
 
     def fetch(self):
         input_value = self.ent.get()
@@ -58,16 +92,42 @@ class Tesla1(Frame):
         if not lookup_result:
             print("invalid")
             self.invalid_pokemon(input_value)
-        else:
-            print("valid")
-            # new pokemon selected
-            self.update_pokemon_name(lookup_result["english"])
-            # sprite updating
-            self.redraw(lookup_result["english"])
-            # base stats updating
-            self.insert_stats(lookup_result["english"])
-            # base defenses updating
-            self.update_defenses(lookup_result["english"])
+            return 0
+
+        pkm = lookup_result["english"]
+
+        # update bottom pokemon
+        if self.state["pokemon"]:
+            self.update_bottom()
+
+        print("valid")
+        # new pokemon selected
+        self.update_pokemon_name(pkm)
+        # sprite updating
+        self.redraw(pkm, "sprite")
+        # base stats updating
+        self.insert_stats(pkm, "stats")
+        # base defenses updating
+        self.update_defenses(pkm, "type_defenses")
+
+        self.state["pokemon"] = pkm
+        self.ent.focus()
+
+
+    def update_bottom(self):
+        # pokemon name
+        text = self.name_label.get("1.0", END)
+        if not "is not a valid pokemon" in text:
+            self.name_label2.delete('1.0', END)
+            self.name_label2.insert('1.0', text)
+
+        pkm = self.state["pokemon"]
+        # stats
+        self.insert_stats(pkm, "stats2")
+        # type_Defenses
+        self.update_defenses(pkm, "type_defenses2")
+        # sprite
+        self.redraw(pkm, "sprite2")
 
     def invalid_pokemon(self, pokemon):
         if pokemon == "Enter Pokemon here [press enter to clear this]":
@@ -96,7 +156,7 @@ class Tesla1(Frame):
         self.name_label.insert('1.0', "["+str(pokedex)+"] "+pokemon.capitalize() + ":"+" "*10+", ".join(loads(type_)))
         self.name_label.config(state=DISABLED)
 
-    def redraw(self, pokemon):
+    def redraw(self, pokemon, label):
         path = "sprites/" + pokemon.lower() + ".png"
         print(path)
         if os.path.exists(path):
@@ -108,11 +168,12 @@ class Tesla1(Frame):
                 os.path.os.remove(path)
                 return
 
-            self.sprite.configure(image=png)
-            self.sprite.image=png
+            sprite = getattr(self, label)
+            sprite.configure(image=png)
+            sprite.image=png
         else:
             self.download_sprite(pokemon)
-            self.redraw(pokemon)
+            self.redraw(pokemon, label)
 
     def download_sprite(self, pokemon):
         """
@@ -133,7 +194,7 @@ class Tesla1(Frame):
         open(path, "wb").write(image.content)
         print("Saved sprite to "+path)
 
-    def insert_stats(self, pokemon):
+    def insert_stats(self, pokemon, label):
         # make sure db is updated and ready
         pokemon = pokemon.lower()
         Downloader(pokemon)
@@ -147,8 +208,10 @@ class Tesla1(Frame):
 
         total = attack + hp + defense + s_attack + s_defense + speed
 
-        self.stats.config(state=NORMAL)
-        self.stats.delete("1.0", END)
+        # yep, the hacking is real
+        stats = getattr(self, label)
+        stats.config(state=NORMAL)
+        stats.delete("1.0", END)
 
         # let's build our text
         text = list()
@@ -163,27 +226,27 @@ class Tesla1(Frame):
         text.append("Total:        "+str(total))
 
         text = "\n".join(text)
-        self.stats.insert("1.0", text)
+        stats.insert("1.0", text)
 
         # let's color some stuff
-        self.stats.tag_add("hp", '2.0', '2.30')
-        self.stats.tag_add("attack", '3.0', '3.30')
-        self.stats.tag_add("defense", '4.0', '4.30')
-        self.stats.tag_add("s_attack", '5.0', '5.30')
-        self.stats.tag_add("s_defense", '6.0', '6.30')
-        self.stats.tag_add("speed", '7.0', '7.30')
-        self.stats.tag_add("total", '9.0', '9.30')
+        stats.tag_add("hp", '2.0', '2.30')
+        stats.tag_add("attack", '3.0', '3.30')
+        stats.tag_add("defense", '4.0', '4.30')
+        stats.tag_add("s_attack", '5.0', '5.30')
+        stats.tag_add("s_defense", '6.0', '6.30')
+        stats.tag_add("speed", '7.0', '7.30')
+        stats.tag_add("total", '9.0', '9.30')
 
 
-        self.stats.tag_config('hp', foreground=self.colorrating(hp))
-        self.stats.tag_config('attack', foreground=self.colorrating(attack))
-        self.stats.tag_config('defense', foreground=self.colorrating(defense))
-        self.stats.tag_config('s_attack', foreground=self.colorrating(s_attack))
-        self.stats.tag_config('s_defense', foreground=self.colorrating(s_defense))
-        self.stats.tag_config('speed', foreground=self.colorrating(speed))
-        self.stats.tag_config('total', foreground=self.colorrating(total/5))
+        stats.tag_config('hp', foreground=self.colorrating(hp))
+        stats.tag_config('attack', foreground=self.colorrating(attack))
+        stats.tag_config('defense', foreground=self.colorrating(defense))
+        stats.tag_config('s_attack', foreground=self.colorrating(s_attack))
+        stats.tag_config('s_defense', foreground=self.colorrating(s_defense))
+        stats.tag_config('speed', foreground=self.colorrating(speed))
+        stats.tag_config('total', foreground=self.colorrating(total/5))
 
-        self.stats.config(state=DISABLED)
+        stats.config(state=DISABLED)
 
     @staticmethod
     def colorrating(value):
@@ -199,7 +262,7 @@ class Tesla1(Frame):
         else:
             return "magenta"
 
-    def update_defenses(self, pokemon):
+    def update_defenses(self, pokemon, label):
         # make sure db is updated and ready
         pokemon = pokemon.lower()
         Downloader(pokemon)
@@ -213,8 +276,9 @@ class Tesla1(Frame):
 
         no_effect, v_resist, resist, weak, v_weak = map(loads, [no_effect, v_resist, resist, weak, v_weak])
 
-        self.type_defenses.config(state=NORMAL)
-        self.type_defenses.delete("1.0", END)
+        type_defenses = getattr(self, label)
+        type_defenses.config(state=NORMAL)
+        type_defenses.delete("1.0", END)
 
         # let's build our text
         text = list()
@@ -226,11 +290,9 @@ class Tesla1(Frame):
         text.append("Very weak:      " + ", ".join(v_weak))
 
         text = "\n".join(text)
-        self.type_defenses.insert("1.0", text)
-        self.type_defenses.config(state=DISABLED)
-
-
-
+        type_defenses.insert("1.0", text)
+        type_defenses.config(state=DISABLED)
 
 if __name__ == "__main__":
-    Tesla1().mainloop()
+
+    t = Tesla1().mainloop()
--- main.py
+++ main.py
@@ -1,42 +1,19 @@
 __author__ = 'zen'
 
 from Database.build_dictionary import download_data
-# from core.pokemon import Pokemon
-# import cmd
-from gui import Tesla1
+from core.gui import Tesla1
+from tkinter import *
+import os
 
 # refresh stuff
 download_data()
 
+# run gui forever
+root = Tk()
+root.iconbitmap(os.path.realpath("Database/Ultra-Ball.ico"))
+root.title("Hi there!")
 
+t = Tesla1(root)
+t.pack()
+root.mainloop()
 
-# class PokemonShell(cmd.Cmd):
-#     intro = "Welcome! Type help or ? to list commands.\n"
-#     prompt = '> '
-#
-#     def default(self, line):
-#         self.do_pokemon(line)
-#
-#     # commands
-#     @staticmethod
-#     def do_pokemon(arg):
-#         """Search for a pokemon.
-#         syntax: pokemon [pokemonname]"""
-#         Pokemon(arg)
-#
-#     def do_p(self, arg):
-#         """Search for a pokemon.
-#         syntax: pokemon [pokemonname]"""
-#         self.do_pokemon(arg)
-#
-#
-#     def do_type(self, arg):
-#         """Search for a type.
-#         syntax: type [typename]"""
-#         pass
-#
-#
-# if __name__ == '__main__':
-#     PokemonShell().cmdloop()
-
-Tesla1().mainloop()
